e8a9ba2d5fc3607294326272be502d6e2467bbdb
matvey <suzdalov@inbox.ru>
2018-12-23 20:35:06 +0300

    new model Project

--- corebook/models.py
+++ corebook/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+import uuid
+from django.utils import timezone
 # Create your models here.
+
+
+class Project(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    dateStart = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
